7fefb1ce27609fa6c0196a24f3a6355022bf1306
ABCXYZ <rnrmfjc@gmail.com>
2022-03-28 19:27:10 +0900

    init : FE. SockJS 및 StompJS 설치 및 세팅

--- BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
+++ BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
@@ -1,11 +1,15 @@
 package prototo.tt.handler;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+@Controller
 public class ReplyEchoHandler extends TextWebSocketHandler {
+    @GetMapping("/replyEcho")
     @Override //커넥션이 연결 되었을때  :  클라이언트가 서버에 접속을 성공했을때 콜백
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         System.out.println("afterConnectionEstablished: " +session);
--- fe/src/App.js
+++ fe/src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import SockJS from 'sockjs-client'
+import Stomp from 'stomp-websocket'
+import Stom from '@stomp/stompjs'
 
 function App() {
+    const connet = () => {
+        // var ws = new SockJS("localhost:8080/replyEcho?bno=1234")
+        var ws = new SockJS("ws://localhost:8080/replyEcho?bno=1234");
+        var stompClient = Stomp.over(ws)
+
+        stompClient.connect({}, function (fame) {
+            // setConnect(true);
+            console.log('Conneted: '+ fame);
+            stompClient.subscribe('/topic/greetings', function (greeting) {
+                console.log(JSON.parse(greeting.body).content);
+            })
+        })
+    }
+
+
   return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+
+          <button onClick={wsButtonClick}>TestBUTTON</button>
+      </>
   );
 }
 
