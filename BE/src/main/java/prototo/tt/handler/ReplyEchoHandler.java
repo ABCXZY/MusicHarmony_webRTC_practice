1ccf403c26215d451332d17a2e75da1776d3aa71
ABCXYZ <rnrmfjc@gmail.com>
2022-03-28 18:18:15 +0900

    feat : RelpyEchoHandler.java 오버라이드(TextWebSocketHandler)

--- BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
+++ BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
@@ -1,7 +1,23 @@
 package prototo.tt.handler;
 
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class ReplyEchoHandler extends TextWebSocketHandler {
+    @Override //커넥션이 연결 되었을때  :  클라이언트가 서버에 접속을 성공했을때 콜백
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
 
+    @Override //소켓에 어떤 메시지를 보냈을때,
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+
+    @Override //커넥션이 클로즈 됐을때,
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
 }
