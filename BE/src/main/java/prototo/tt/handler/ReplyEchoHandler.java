f601128553fef31596fdaecf8939a5ba9b428545
ABCXYZ <rnrmfjc@gmail.com>
2022-03-28 18:27:14 +0900

    chore : 소켓이 연결될때 문자열 출력 테스트

--- BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
+++ BE/src/main/java/prototo/tt/handler/ReplyEchoHandler.java
@@ -8,16 +8,16 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 public class ReplyEchoHandler extends TextWebSocketHandler {
     @Override //커넥션이 연결 되었을때  :  클라이언트가 서버에 접속을 성공했을때 콜백
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        super.afterConnectionEstablished(session);
+        System.out.println("afterConnectionEstablished: " +session);
     }
 
     @Override //소켓에 어떤 메시지를 보냈을때,
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        super.handleTextMessage(session, message);
+        System.out.println("handle");
     }
 
     @Override //커넥션이 클로즈 됐을때,
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        super.afterConnectionClosed(session, status);
+        System.out.println("close");
     }
 }
